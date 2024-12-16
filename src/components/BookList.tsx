157e9fc39360ac2445879bd7a70ff0ece07e4819
siwon99 <kimsiwon0707@gmail.com>
2024-12-16 21:57:11 +0900

    feat: api 코드

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BookCard } from './BookCard';
 import { Pagination } from './Pagination';
 import { SearchBar } from './SearchBar';
+import { fetchBook } from '../services/bookservice';
+import { Book } from '../types/bookTypes';
 
 const BookList: React.FC = () => {
-  const [books, setBooks] = useState([]); //전체 책
-  const [filteredBooks, setFilteredBooks] = useState([]);
+  const [books, setBooks] = useState<Book[]>([]);
+  const [filteredBooks, setFilteredBooks] = useState<Book[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchBar, setSearchBar] = useState('');
+
+  useEffect(() => {
+    const getBook = async () => {
+      const data = await fetchBook();
+      setBooks(data);
+      setFilteredBooks(data);
+    };
+    getBook();
+  }, []);
 
   return (
     <>
