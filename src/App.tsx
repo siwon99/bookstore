64328bd87065782288589c2e54b515abff0682c6
siwon99 <kimsiwon0707@gmail.com>
2024-12-17 19:43:09 +0900

    fix: import문 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { BookList } from './components/BookList';
-import { BookDetail } from './components/BookDetail';
+import BookList from './components/BookList';
+import BookDetail from './components/BookDetail';
 
 const App: React.FC = () => {
   return (
