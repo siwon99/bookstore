5ea8d03fed65400fb470d6397f749f560a2c4fa9
siwon99 <kimsiwon0707@gmail.com>
2024-12-17 19:26:27 +0900

    feat: 책 정보 및 ID 함수 작성

--- src/services/bookservice.ts
+++ src/services/bookservice.ts
@@ -2,7 +2,14 @@ import { Book } from '../types/bookTypes';
 
 const BASE_URL = '/api/books';
 
+// 책 정보 가져오기
 export const fetchBook = async (): Promise<Book[]> => {
   const response = await fetch(BASE_URL);
+  return await response.json();
+};
+
+// 책 ID 가져오기
+export const fetchBookById = async (id: string): Promise<Book> => {
+  const response = await fetch(`${BASE_URL}/${id}`);
   return await response.json();
 };
