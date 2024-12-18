7966feef08d45e9cf286aa444e3e84e1cfc6df3f
siwon99 <kimsiwon0707@gmail.com>
2024-12-18 15:27:54 +0900

    fix: BookDetail 컴포넌트 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BookList from './components/BookList';
-import BookDetail from './components/BookDetail';
-import Admin from './components/Admin';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<BookList />} />
-        <Route path="/book/:bookId" element={<BookDetail />} />
-        <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
   );
